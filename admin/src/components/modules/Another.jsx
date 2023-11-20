14e8455690c55a8c131c509363e5dc1c1da9e09a
Tapan Kumer Das <voboghure.dev@gmail.com>
2023-11-20 08:11:15 +0600

    Remove editorconfig for project, reformat, and add directory for catgory

--- admin/src/components/modules/Another.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Another = () => {
-    return <div>Another</div>;
-};
-
-export default Another;
--- admin/src/components/routes/Router.jsx
+++ admin/src/components/routes/Router.jsx
@@ -1,60 +1,60 @@
-import { createBrowserRouter } from "react-router-dom";
-import Master from "../layout/Master";
-import Dashboard from "../modules/Dashboard";
-import Login from "../modules/Login";
-import Auth from "../layout/Auth";
-import CategoryList from "../modules/CategoryList";
-import Error500 from "../modules/Error500";
-import Test from "../modules/Test";
-import Public from "./Public";
-import Protected from "./Protected";
-import CategoryAdd from "../modules/CategoryAdd";
+import { createBrowserRouter } from 'react-router-dom';
+import Auth from '../layout/Auth';
+import Login from '../modules/Login';
+import Master from '../layout/Master';
+import Dashboard from '../modules/Dashboard';
+import Error500 from '../modules/Error500';
+import Public from './Public';
+import Protected from './Protected';
+import CategoryList from '../modules/category/CategoryList';
+import CategoryAdd from '../modules/category/CategoryAdd';
+import CategoryEdit from '../modules/category/CategoryEdit';
 
 const Router = createBrowserRouter([
-    {
-        path: "/dashboard",
-        element: (
-            <Protected>
-                <Master />
-            </Protected>
-        ),
-        children: [
-            {
-                path: "/dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "/dashboard/category/create",
-                element: <CategoryAdd />,
-            },
-            {
-                path: "/dashboard/category",
-                element: <CategoryList />,
-            },
-            {
-                path: "/dashboard/error-500",
-                element: <Error500 />,
-            },
-        ],
-    },
-    {
-        path: "/",
-        element: (
-            <Public>
-                <Auth />
-            </Public>
-        ),
-        children: [
-            {
-                path: "/",
-                element: <Login />,
-            },
-            {
-                path: "/test",
-                element: <Test />,
-            },
-        ],
-    },
+	{
+		path: '/dashboard',
+		element: (
+			<Protected>
+				<Master />
+			</Protected>
+		),
+		children: [
+			{
+				path: '/dashboard',
+				element: <Dashboard />,
+			},
+			{
+				path: '/dashboard/category/create',
+				element: <CategoryAdd />,
+			},
+			{
+				path: '/dashboard/category/edit/:id',
+				element: <CategoryEdit />,
+			},
+			{
+				path: '/dashboard/category',
+				element: <CategoryList />,
+			},
+			{
+				path: '/dashboard/error-500',
+				element: <Error500 />,
+			},
+		],
+	},
+	{
+		path: '/',
+		element: (
+			<Public>
+				<Auth />
+			</Public>
+		),
+		children: [
+			{
+				path: '/',
+				element: <Login />,
+			},
+		],
+	},
 ]);
 
 export default Router;
